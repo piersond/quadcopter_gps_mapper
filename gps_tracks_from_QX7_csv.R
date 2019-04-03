6c4e85c6836b25ff9c64941da10d798868db9d56
Derek Pierson <drk.pierson@gmail.com>
2019-04-02 21:53:28 -0700

    Atom io test commit. Minor format change.

--- gps_tracks_from_QX7_csv.R
+++ gps_tracks_from_QX7_csv.R
@@ -13,7 +13,7 @@ register_google(key = API_key, write = TRUE)
 setwd("C:/github/quadcopter_gps")
 
 #Read in data csv from QX7
-gps <- read.csv("super_g-2019-03-29.csv", as.is=T, na.strings=c("","NA")) 
+gps <- read.csv("super_g-2019-03-29.csv", as.is=T, na.strings=c("","NA"))
 
 #Split GPS string into lat and lon
 gps <- gps %>% separate(GPS, c("lat", "lon"), " ", extra = "merge")
@@ -28,6 +28,6 @@ gps$lon <- as.numeric(gps$lon)
 write.csv(gps, "gps_from_R.csv")
 
 #Map GPS path
-base_map <- get_map(location = c(median(gps$lon), median(gps$lat)), maptype = "satellite", source = "google", zoom = 15)
+base_map <- get_map(location = c(median(gps$lon), median(gps$lat)),
+  maptype = "satellite", source = "google", zoom = 15)
 ggmap(base_map) + geom_path(data = gps, color="red")
-
